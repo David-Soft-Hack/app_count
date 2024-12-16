9a80ca31fdd54435a10bd98df9550f247a04fea8
David <davidsoft97@gmail.com>
2024-12-16 17:29:46 -0600

    Punto Inicial

---
+++ project_flutter/lib/main.dart
@@ -0,0 +1,15 @@
+//Dependency Material 3
+import 'package:flutter/material.dart';
+
+//Point Start
+void main()=>runApp(const AppMain());
+
+//Define Screen Main
+class AppMain extends StatelessWidget {
+  const AppMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
